d9d4a90e486bb6cdc2b40f1d9a8071b654f8309d
shunsuke <shunsuke@shunsuke-VirtualBox.(none)>
2013-12-27 00:25:24 +0900

    add Enemycharacter.cpp

---
+++ src/EnemyCharacter.cpp
@@ -0,0 +1,21 @@
+#include "EnemyCharacter.h"
+
+EnemyCharacter::EnemyCharacter(string name, SyukatsuGame *game)
+  :PlayerCharacter(name, game)
+{
+  this->setAttributes(10, 10, 10, 10.0f, Vector3(1.0f, 0.0f, 0.0f)); 
+}
+
+void EnemyCharacter::update(float deltaTime)
+{  
+  if( position.distanceTo(distination) < speed*deltaTime )
+  {
+    setStatus(Actor::Dead); //たどり着いたら死ぬ    
+    return;    
+  }
+  direction = distination - position;
+  direction.normalize();
+  
+  position += direction*deltaTime*speed;  
+}
+
