433b823fdd3d16255faed5a638e1ffe352f3eea5
yotta2 <shunsuketakano@Shunsuke-no-MacBook-Pro.local>
2013-12-31 01:48:54 +0900

    add LightningTower

---
+++ src/LightningTower.cpp
@@ -0,0 +1,13 @@
+#include "LightningTower.h"
+
+LightningTower::LightningTower(string _name, SyukatsuGame *_game, Field *_field, CharacterManager *_cmanager)
+  :Building(_name, _game, _field), cmanager(_cmanager), timer(0)
+{
+}
+
+void LightningTower::update(float deltaTime)
+{
+  time += deltaTime;
+  if(timer >= attackrate) {
+  }
+}
