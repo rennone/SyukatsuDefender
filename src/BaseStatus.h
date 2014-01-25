9b6b66a4eeae84acb82b943e5f27671d2ce6384e
shunsuke <shunsuke@shunsuke-VirtualBox.(none)>
2014-01-26 02:31:05 +0900

    WIP：BaseStatusクラスの実装

---
+++ src/BaseStatus.h
@@ -0,0 +1,45 @@
+#include "Information.h"
+#include <map>
+#include <string>
+
+class CharacterBaseStatus
+{
+  protected:
+    std::map<std::string, float> status;
+
+  public: 
+    CharacterBaseStatus();
+
+    int getMaxhp();
+    float getSpeed();
+    int getAttack(); 
+    int getDefense(); 
+    
+    void load(int characterId);
+};
+
+class BuildingBaseStatus
+{
+  protected:
+    std::map<std::string, float> status;
+
+  public:
+    BuildingBaseStatus();
+    
+    int getAttack();
+    int getBaseValue();
+    float getRangeOfEffect();
+    float getAttackRate();
+    
+    void load(int buildingId);
+};
+
+class BaseStatus
+{
+  protected:
+    CharacterBaseStatus *characterBaseStatus;
+    BuildingBaseStatus *buildingBaseStatus;
+
+  public:
+    BaseStatus();
+};
